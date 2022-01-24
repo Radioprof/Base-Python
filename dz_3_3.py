fbe7306a5520401cc1d365054918eb4605d3f507
Radioprof <so.bunin@gmail.com>
2022-01-24 14:02:02 +0300

    Занятие №3, задание 3.

---
+++ dz_3_3.py
@@ -0,0 +1,12 @@
+def thesaurus(*args):
+    notebook = {}
+    for name in sorted(args):
+        key = name[0]
+        if notebook.get(key) is None:
+            notebook.update({key:[name]})
+        else:
+            notebook[key] += [name]
+    print(notebook)
+
+
+thesaurus('Ivan', 'Petr', 'Lesha', 'Ilia', 'Ira', 'Lena', 'Larisa', 'Pavel')
