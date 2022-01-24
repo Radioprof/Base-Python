6197e5a8b5ef396c70047d7f1061926d94c51cb3
Radioprof <so.bunin@gmail.com>
2022-01-24 14:03:13 +0300

    Занятие №3, задание 5.

---
+++ dz_3_5.py
@@ -0,0 +1,24 @@
+from random import randrange, choice
+
+
+def get_jokes(numb, not_rep=True):
+    """
+    Generate jokes from inner lists of words
+
+    :param numb: number of jokes
+    :param not_rep: False-words in jokes can repit, True -words in jokes don't repit
+    :return: list of random jokes
+
+    """
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+    if not_rep and len(min(nouns,adverbs, adjectives)) < numb:
+        numb = len(min(nouns,adverbs, adjectives))
+    for i in range(numb):
+        if not_rep:
+            print(f'{nouns.pop(randrange(len(nouns)))} {adverbs.pop(randrange(len(adverbs)))} {adjectives.pop(randrange(len(adjectives)))}')
+        else:
+            print(f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}')
+
+get_jokes(10)
